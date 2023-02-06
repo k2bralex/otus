ffba947e5783305962a310db8e89dcdc85ae0349
alex <k2.bralex@gmail.com>
2023-02-06 15:57:45 +0300

    hw_4 done

--- main.go
+++ main.go
@@ -9,22 +9,41 @@ import (
 
 	*/
 
-	"Otus/hw_3"
+	"Otus/hw_4"
+	"fmt"
 )
 
 func main() {
 
-	//HM_3
+	//HM_4
+	list := hw_4.DoublyLinkedList{}
+	for i := 0; i < 100; i += 15 {
+		list.PushBack(i)
+	}
 
-	str := "Firstly, similar to the sorting by key method, we have to obtain a slice of all the keys. " +
-		"Now we want to sort the keys according to the values, for doing that, we use the SliceStable " +
-		"function in the sort module. The slices table function takes in the slice and we can provide " +
-		"less function. We can simply provide an anonymous lambda fiction that checks for the " +
-		"comparison of the values of the provided slice."
+	for ptr := list.GetHead(); ptr != nil; ptr = ptr.GetNext() {
+		fmt.Println(ptr.GetData())
+	}
 
-	hw_3.TopStringWords(str, 10)
+	for i := -100; i < 0; i += 15 {
+		list.PushFront(i)
+	}
+
+	for ptr := list.GetHead(); ptr != nil; ptr = ptr.GetNext() {
+		fmt.Println(ptr.GetData())
+	}
 
 	/*
+
+		//HM_3
+
+		str := "Firstly, similar to the sorting by key method, we have to obtain a slice of all the keys. " +
+			"Now we want to sort the keys according to the values, for doing that, we use the SliceStable " +
+			"function in the sort module. The slices table function takes in the slice and we can provide " +
+			"less function. We can simply provide an anonymous lambda fiction that checks for the " +
+			"comparison of the values of the provided slice."
+
+		hw_3.TopStringWords(str, 10)
 
 		//HM_2
 		fmt.Println(hw_2.StringParse("a4bc2d5e"))
